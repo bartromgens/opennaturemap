cf12cb8d27d470277600d301b38927b6eca75b5a
Bart Römgens <bart.romgens@gmail.com>
2026-02-15 10:02:52 +0100

    Configure loggers

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,27 @@ REST_FRAMEWORK = {
     ],
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 try:
     from .settings_local import *
 except ImportError:
