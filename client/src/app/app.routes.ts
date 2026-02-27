b9e7e378e69c7209b9d12fa92cfe72092d61af8d
Bart Römgens <bart.romgens@gmail.com>
2026-02-27 16:33:21 +0100

    Add basic SEO: meta tags, robots.txt, sitemap, per-route titles

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'OpenNatureMap - Nature reserves on an interactive map',
     loadComponent: () => import('./map/map.component').then((m) => m.MapComponent),
   },
   {
     path: 'protection-classes',
+    title: 'Protection Classes - OpenNatureMap',
     loadComponent: () =>
       import('./protection-classes/protection-classes.component').then(
         (m) => m.ProtectionClassesComponent,
